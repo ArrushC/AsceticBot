231ef0dc7ebdc8ce18fe260274090a9bc19824d6
Arrush <arrush240@gmail.com>
2019-08-11 00:07:25 +0100

    I think guild database should be setup by now.

--- src/main/kotlin/com/arrush/ascetic/internal/database/guild/GuildData.kt
+++ src/main/kotlin/com/arrush/ascetic/internal/database/guild/GuildData.kt
@@ -3,6 +3,6 @@ package com.arrush.ascetic.internal.database.guild
 import com.arrush.ascetic.Constants
 import me.arrush.javabase.annotations.SQLColumn
 
-class GuildData (@SQLColumn(column="id") var guildId: Long= 0, @SQLColumn(column="prefix") var prefix: String= Constants.PREFIX.getString()) {
+class GuildData (@SQLColumn(column="id") val guildId: Long= 0, @SQLColumn(column="prefix") var prefix: String= Constants.PREFIX.getString()) {
 
 }
--- src/main/kotlin/com/arrush/ascetic/internal/database/guild/GuildDatabase.kt
+++ src/main/kotlin/com/arrush/ascetic/internal/database/guild/GuildDatabase.kt
@@ -2,62 +2,66 @@ package com.arrush.ascetic.internal.database.guild
 
 import com.arrush.ascetic.AsceticBot
 import com.arrush.ascetic.Constants
-import discord4j.core.`object`.util.Snowflake
 import discord4j.core.event.domain.lifecycle.ReadyEvent
+import me.arrush.javabase.annotations.SQLColumn
 import me.arrush.javabase.entities.Column
 import me.arrush.javabase.entities.Row
 import me.arrush.javabase.entities.Table
 import me.arrush.javabase.enums.DataType
+import kotlin.reflect.full.declaredMemberProperties
+import kotlin.reflect.full.findAnnotation
 
 @Suppress("MemberVisibilityCanBePrivate")
 enum class GuildDatabase {
+
+    // The instance.
     INSTANCE;
 
     val tableName: String = "Guilds"
     val columns: Map<String, Column> = mapOf(
-            "id" to Column("id", DataType.BIGINT)
+            "id" to Column("id", DataType.BIGINT),
+            "prefix" to Column("prefix", DataType.TEXT)
     )
     val guildTable: Table = Table.TableCreator(this.tableName).addColumns(columns.values).build(AsceticBot.INSTANCE.database)
     val guilds: MutableMap<Long, GuildData> = mutableMapOf()
 
-
-    fun addGuild(id: Long) {
-        this.guilds.putIfAbsent(id, GuildData(id, Constants.PREFIX.getString()))
+    init {
+        // loading guild data and saving it into map.
+        this.loadData()
     }
 
-    fun getGuild(id: Snowflake): GuildData {
-        val gData = GuildData(id.asLong())
-        this.guildTable.select().withStatement("SELECT * FROM ${this.tableName} WHERE id == ${id.asLong()}")
-                .fetch().parse(gData.javaClass)
-        return gData
-    }
-
-    fun loadData(): MutableMap<Long, GuildData> {
+    // This simply loads data from the database, preventing overwritten data
+    // It is void to retain immutability of the map.
+    fun loadData() {
         val rows: List<Row> = this.guildTable.select().withStatement("SELECT * FROM ${this.tableName}").fetchMultiple()
         for (row in rows) {
             val gd = GuildData()
             row.parse(gd.javaClass)
             this.guilds.putIfAbsent(row.getLong("id"), gd)
         }
-        return this.guilds
     }
 
+    // each method in GuildData will return the instance itself so it will be easier to modify.
+    fun modifyGuild(data: GuildData) {
+        val fieldValues: Map<String, Any> = GuildData::class.declaredMemberProperties.filter { it.findAnnotation<SQLColumn>() != null }
+                .map { it.findAnnotation<SQLColumn>()!!.column to it.get(data)!! }.toMap() // Map<ColumnName, ColumnValue>
+        this.guildTable.update().withStatement(this.updateStatement(fieldValues)).queue()
+        this.guilds.replace(data.guildId, data)
+    }
+
+    // Saving guilds that do not exist in the database.
     fun saveGuild(guild: ReadyEvent.Guild) {
         if (!guild.isAvailable) return
-        // save it in to the actual database.
-        // replace this statement with a query.
-        this.guilds.putIfAbsent(guild.id.asLong(), GuildData(guild.id.asLong(), Constants.PREFIX.getString()))
+        val data = GuildData(guild.id.asLong(), Constants.PREFIX.getString())
+        this.guildTable.insert().withStatement(this.insertStatement(data)).queue()
+        this.guilds.putIfAbsent(guild.id.asLong(), data)
     }
-    
     fun saveGuilds(guilds: Collection<ReadyEvent.Guild>) {
         for (guild in guilds) {
             this.saveGuild(guild)
         }
     }
 
-    // function for modifying data.
-
-
-
-
+    private fun updateStatement(fieldValues: Map<String, Any>) = "UPDATE ${this.tableName} SET ${fieldValues.entries.joinToString { it.key + "=" + it.value }} WHERE id == ${fieldValues["id"]}"
+    private fun insertStatement(data: GuildData): String = "INSERT INTO ${this.tableName} VALUES (${data.guildId}, ${data.prefix})"
 }
